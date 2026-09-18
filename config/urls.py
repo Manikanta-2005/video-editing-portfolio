3c22a7721ee91881003fe7ba6dbb8606c3b5d8ad
Bandaru Manikanta <bandarumanikanta1319@gmail.com>
2026-09-18 11:44:54 +0530

    Fix production media serving

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
-]
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+    # Serve uploaded media files
+    path(
+        "media/<path:path>",
+        serve,
+        {
+            "document_root": settings.MEDIA_ROOT,
+        },
+    ),
+]
